305d68e224e65b1ca1ce5081b94537114b15d578
Filipe Virtuoso <lfvirtuoso@gmail.com>
2023-07-04 08:14:25 -0300

    Update

--- src/components/PageSelector/styled.js
+++ src/components/PageSelector/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   width: 100%;
-  background-color: red;
   display: flex;
   justify-content: center;
   flew-wrap: wrap;
