6c4df755a8d9fa770e0b36a68407eae98d2aa509
Filipe Virtuoso <lfvirtuoso@gmail.com>
2023-07-18 10:28:23 -0300

    Update

--- src/screens/Map.jsx
+++ src/screens/Map.jsx
@@ -3,10 +3,21 @@ import React from 'react'
 // Leaflet
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import L from "leaflet";
+
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
 
 // Style
 import styled from 'styled-components'
 import Tribal from '../assets/images/bg-home.png'
+
+// RTK
+import { useGetEventsQuery } from '../slices/eventsApiSlice'
+
+// Icons
+import PinIcon from '../assets/images/map-pin-solid.svg'
 
 const Container = styled.section`
   width: 100%;
@@ -27,7 +38,18 @@ const TribalBG = styled.section`
   opacity: 0.3;
 `
 
+const P = styled.div`
+  margin: 0;
+  padding: 0;
+
+`
+
 const Map = () => {
+
+  const Pin = L.icon({ iconUrl: PinIcon, iconSize: [18, 18], MarkerColor: 'red' });
+  const { data: response, isLoading, error } = useGetEventsQuery();
+
+  console.log(response)
 
   const mapContainerStyle = {
     // marginTop: "5rem",
@@ -38,19 +60,25 @@ const Map = () => {
   const position = [-14.2400732, -53.1805017]
   return (
     <>
-        <TribalBG />
-    <MapContainer style={mapContainerStyle}  center={position}  zoom={3}  scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
-    </MapContainer>
-    <TribalBG />
+      { isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+        <>
+          <TribalBG />
+          <MapContainer style={mapContainerStyle}  center={position}  zoom={3}  scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {response.map((event) => (
+              <Marker position={event.coordinates.split(',')} icon={Pin}>
+                <Popup>
+                  <P>{event.description}</P>
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+          <TribalBG />
+        </>
+      )}
     </>
   )
 }
