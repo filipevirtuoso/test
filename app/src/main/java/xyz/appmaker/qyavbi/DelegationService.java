1a09ebd34ff11eef1cb598377305b77786a27bab
Filipe Virtuoso <lfvirtuoso@gmail.com>
2023-10-13 09:26:29 -0300

    update

---
+++ app/src/main/java/xyz/appmaker/qyavbi/DelegationService.java
@@ -0,0 +1,18 @@
+package xyz.appmaker.qyavbi;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
