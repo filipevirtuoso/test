32be9fd3d131f9bd4f56fd04cdfa981aa9cc82ab
Filipe Virtuoso <lfvirtuoso@gmail.com>
2023-11-21 20:56:52 -0300

    Possible fix: Async error when testing events.length

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 // Icons
-import { FaUnderline, FaUserCircle } from 'react-icons/fa'
+import { FaUnderline, FaUserCircle } from 'react-icons/fa';
 
 //Style
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import infoBG from '../assets/images/profile-bg-22.png'
+import infoBG from '../assets/images/profile-bg-22.png';
 
 // RTK
 import { useUserDetailQuery } from '../slices/usersSlice';
-import { useGetEventsQuery } from '../slices/eventsApiSlice'
+import { useGetEventsQuery } from '../slices/eventsApiSlice';
 import { useGetStatisticsQuery } from '../slices/eventsApiSlice';
 
 // Router
 import { Link } from 'react-router-dom';
 
 // Components
-import Message from '../components/Message'
+import Message from '../components/Message';
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-import BGImage from '../assets/images/owl.png'
+import BGImage from '../assets/images/owl.png';
 import { Spinner } from 'react-bootstrap';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -35,7 +35,7 @@ const UserInfoWrapper = styled.section`
   padding-left: 2rem;
   border-bottom: 2px solid #444;
   background-color: #000;
-`
+`;
 
 const NameWrapper = styled.section`
   display: flex;
@@ -45,28 +45,27 @@ const NameWrapper = styled.section`
   // align-items: center;
   margin-left: 1rem;
   color: #fff;
-`
+`;
 
 const Name = styled.span`
   margin: 0;
   padding: 0;
   font-weight: bold;
   color: #fff;
-`
+`;
 
 const Name2 = styled.span`
   margin: 0;
   padding: 0;
-`
-
+`;
 
 const InfoWrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  // background-color: #000;     
-  background-image: url(${infoBG}); 
+  // background-color: #000;
+  background-image: url(${infoBG});
   background-repeat: no-repeat;
   background-size: cover;
 
@@ -74,11 +73,8 @@ const InfoWrapper = styled.section`
   // background-color: #2f663d;
   height: 25vh;
   width: 100%;
-  color: #444 ;
-
-  
-
-`
+  color: #444;
+`;
 
 const LeftWrapper = styled.section`
   width: 50%;
@@ -88,8 +84,8 @@ const LeftWrapper = styled.section`
   // flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: #f3f3f3
-`
+  color: #f3f3f3;
+`;
 
 const RightWrapper = styled.section`
   width: 100%;
@@ -100,12 +96,7 @@ const RightWrapper = styled.section`
   display: flex;
   justify-content: space-evenly;
   margin: 3rem;
-
-
-
-
-
-`
+`;
 
 const Titlee = styled.h2`
   font-size: 1.2rem;
@@ -115,7 +106,7 @@ const Titlee = styled.h2`
   // margin-top: .7rem;
 
   margin-bottom: 0;
-`
+`;
 
 const Div = styled.div`
   width: 100%;
@@ -125,27 +116,26 @@ const Div = styled.div`
   justify-content: center;
   align-items: flex-start;
   // background-color: blue;
-  font-size: .9rem;
-`
+  font-size: 0.9rem;
+`;
 
 const P = styled.p`
   text-decoration: underline;
   // font-weight:
   // margin: 0;
   padding: 0;
-`
+`;
 
 const Image = styled.section`
   width: 30px;
   height: 50px;
   position: fixed;
   right: 0;
-  background-image: url(${BGImage}); 
+  background-image: url(${BGImage});
   background-repeat: no-repeat;
   background-size: contain;
   opacity: 0.3;
-`
-
+`;
 
 const Red = styled.div`
   width: 10px;
@@ -153,7 +143,7 @@ const Red = styled.div`
   background-color: rgba(255, 99, 132, 0.9);
   color: #f2f2f2;
   font-weight: bold;
-`
+`;
 
 const Blue = styled.div`
   width: 10px;
@@ -161,7 +151,7 @@ const Blue = styled.div`
   background-color: rgba(54, 162, 235, 0.9);
   color: #f2f2f2;
   font-weight: bold;
-`
+`;
 
 const Yellow = styled.div`
   width: 10px;
@@ -169,7 +159,7 @@ const Yellow = styled.div`
   background-color: rgba(255, 206, 86, 0.9);
   color: #f2f2f2;
   font-weight: bold;
-`
+`;
 
 const Green = styled.div`
   width: 10px;
@@ -177,15 +167,14 @@ const Green = styled.div`
   background-color: rgba(75, 192, 192, 0.9);
   color: #f2f2f2;
   font-weight: bold;
-`
+`;
 const DD = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: .3rem;
-  font-size: .8rem;
-
-`
+  gap: 0.3rem;
+  font-size: 0.8rem;
+`;
 
 const Coolor = styled.div`
   width: 10px;
@@ -193,7 +182,7 @@ const Coolor = styled.div`
   background-color: rgba(153, 102, 255, 0.9);
   color: #f2f2f2;
   font-weight: bold;
-`
+`;
 
 const Cooolor = styled.div`
   width: 10px;
@@ -201,106 +190,134 @@ const Cooolor = styled.div`
   background-color: rgba(255, 159, 64, 0.9);
   color: #f2f2f2;
   font-weight: bold;
-`
+`;
 
+const Header = ({ name, indigenous_territory }) => {
+  const { data, isLoading, error } = useUserDetailQuery();
 
-
-const Header = ({ name, indigenous_territory  }) => {
-
-  
-
-
-
-  const { data, isLoading, error} = useUserDetailQuery();
-
-  const {data: events, isLoading: isLoadingEvents, error: eventErrors} = useGetEventsQuery();
-  const  {data: stats, isLoading: isLoadingStats, error: statsErros} = useGetStatisticsQuery();
-  const [chartInfo, setChartInfo] = useState('')
-  const [lang, setLang] = useState('')
-
+  const {
+    data: events,
+    isLoading: isLoadingEvents,
+    error: eventErrors,
+  } = useGetEventsQuery();
+  const {
+    data: stats,
+    isLoading: isLoadingStats,
+    error: statsErros,
+  } = useGetStatisticsQuery();
+  const [chartInfo, setChartInfo] = useState('');
+  const [lang, setLang] = useState('');
 
   useEffect(() => {
-    const lastSelected = JSON.parse(
-      localStorage.getItem("lang") ?? "[]"
-    );
+    const lastSelected = JSON.parse(localStorage.getItem('lang') ?? '[]');
     setLang(lastSelected);
-  }, [])
+  }, []);
 
   useEffect(() => {
-
-    if(stats) {
-       setChartInfo({datasets: [
-        {
-          label: 'Ocorrências:',
-          data: [stats.total_desmate, stats.total_garimpo, stats.total_grilagem, stats.total_invasão, stats.total_queimadas, stats.total_outros],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.9)',
-            'rgba(54, 162, 235, 0.9)',
-            'rgba(255, 206, 86, 0.9)',
-            'rgba(75, 192, 192, 0.9)',
-            'rgba(153, 102, 255, 0.9)',
-            'rgba(255, 159, 64, 0.9)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
-        },
-      ],})
+    if (stats) {
+      setChartInfo({
+        datasets: [
+          {
+            label: 'Ocorrências:',
+            data: [
+              stats.total_desmate,
+              stats.total_garimpo,
+              stats.total_grilagem,
+              stats.total_invasão,
+              stats.total_queimadas,
+              stats.total_outros,
+            ],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.9)',
+              'rgba(54, 162, 235, 0.9)',
+              'rgba(255, 206, 86, 0.9)',
+              'rgba(75, 192, 192, 0.9)',
+              'rgba(153, 102, 255, 0.9)',
+              'rgba(255, 159, 64, 0.9)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      });
     }
+  }, stats);
 
-    
-  }, stats)
+  console.log(lang);
 
-
-  console.log(lang)
-
-  
   return (
     <>
-    { isLoading ? '': error ? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) : (
-      <>
-      <UserInfoWrapper>
-        <Link to='/profile'>
-          <FaUserCircle color={"#EB6900"} size={35} />
-        </Link>
-        <NameWrapper>
-          <Name>{data.name}</Name>
-          <Name2>Colaborador</Name2>
-        </NameWrapper>
-        {/* <Image /> */}
-      </UserInfoWrapper>
+      {isLoading ? (
+        ''
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <UserInfoWrapper>
+            <Link to='/profile'>
+              <FaUserCircle color={'#EB6900'} size={35} />
+            </Link>
+            <NameWrapper>
+              <Name>{data.name}</Name>
+              <Name2>Colaborador</Name2>
+            </NameWrapper>
+            {/* <Image /> */}
+          </UserInfoWrapper>
 
-      <InfoWrapper>
-
-
-        { !!chartInfo === false ? '': error ? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) : (
-          <RightWrapper>
-          <Pie data={chartInfo} />
-          <Div>
-          <DD><Red />{lang === "Português" ? "Desmate" : "Urihi peprai"}</DD>
-          <DD><Blue />{lang === "Português" ? "Garimpo" : "Makatahu Xii Praãhawë Teaiwei"}</DD>
-          <DD><Yellow />{lang === "Português" ? "Grilagem" : "Urihi tomiai"}</DD>
-          <DD><Green />{lang === "Português" ? "Invasão" : "Rukëi Katiowei"}</DD>
-          <DD><Coolor />{lang === "Português" ? "Queimadas" : "Wakë Yaëaiwei"}</DD>
-          <DD><Cooolor />{lang === "Português" ? "Outros" : "Ai thëpe"}</DD>
-          </Div>
-          </RightWrapper>
-        )}
-
-
-
-
-      </InfoWrapper>
-      </>
-      )} 
+          <InfoWrapper>
+            {!!chartInfo === false ? (
+              ''
+            ) : error ? (
+              <Message variant='danger'>
+                {error?.data?.message || error.error}
+              </Message>
+            ) : (
+              <RightWrapper>
+                <Pie data={chartInfo} />
+                <Div>
+                  <DD>
+                    <Red />
+                    {lang === 'Português' ? 'Desmate' : 'Urihi peprai'}
+                  </DD>
+                  <DD>
+                    <Blue />
+                    {lang === 'Português'
+                      ? 'Garimpo'
+                      : 'Makatahu Xii Praãhawë Teaiwei'}
+                  </DD>
+                  <DD>
+                    <Yellow />
+                    {lang === 'Português' ? 'Grilagem' : 'Urihi tomiai'}
+                  </DD>
+                  <DD>
+                    <Green />
+                    {lang === 'Português' ? 'Invasão' : 'Rukëi Katiowei'}
+                  </DD>
+                  <DD>
+                    <Coolor />
+                    {lang === 'Português' ? 'Queimadas' : 'Wakë Yaëaiwei'}
+                  </DD>
+                  <DD>
+                    <Cooolor />
+                    {lang === 'Português' ? 'Outros' : 'Ai thëpe'}
+                  </DD>
+                </Div>
+              </RightWrapper>
+            )}
+          </InfoWrapper>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 // Style
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 // Components
-import PageSelector from '../components/PageSelector'
-import EventCard from '../components/EventCard'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
+import PageSelector from '../components/PageSelector';
+import EventCard from '../components/EventCard';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 // RTK
-import { useGetEventsQuery } from '../slices/eventsApiSlice'
+import { useGetEventsQuery } from '../slices/eventsApiSlice';
 
 // Other
-import paginate from '../utils/paginate'
-import BGImage from '../assets/images/bg-home.png'
+import paginate from '../utils/paginate';
+import BGImage from '../assets/images/bg-home.png';
 
-import conaferLogo from '../assets/images/conafer-logo.webp'
-import GuardiansLogo from '../assets/images/1.png'
-import BrigadasLogo from '../assets/images/3.png'
-import TerraLogo from '../assets/images/2.png'
+import conaferLogo from '../assets/images/conafer-logo.webp';
+import GuardiansLogo from '../assets/images/1.png';
+import BrigadasLogo from '../assets/images/3.png';
+import TerraLogo from '../assets/images/2.png';
 
 import Carousel from 'react-bootstrap/Carousel';
 import Table from 'react-bootstrap/Table';
@@ -33,7 +33,7 @@ const EventsWrapper = styled.section`
   justify-content: flex-start;
   align-items: center;
   background-color: #000;
-`
+`;
 
 const Events = styled.section`
   width: 100%;
@@ -42,75 +42,75 @@ const Events = styled.section`
   justify-content: center;
   align-items: flex-start;
   // background-color: red;
-`
+`;
 
 const Image = styled.section`
   width: 100%;
   height: 8vh;
   display: flex;
   justify-content: center;
-  margin-top: .5rem;
+  margin-top: 0.5rem;
 
-  background-image: url(${BGImage}); 
+  background-image: url(${BGImage});
   background-repeat: no-repeat;
   background-size: contain;
   opacity: 0.3;
-`
+`;
 
 const Th = styled.th`
   border: 2px solid black;
   border-collapse: collapse;
   padding: 5px;
-text-align: left;
-`
+  text-align: left;
+`;
 
 const Td = styled.td`
   border: 2px solid black;
   border-collapse: collapse;
   padding: 5px;
-text-align: left;
-`
+  text-align: left;
+`;
 
 const linkStyle = {
-  textDecoration: "none",
+  textDecoration: 'none',
   // display: "flex",
   // alignItems: "center",
-  color: '#444'
+  color: '#444',
 };
 
 const Wrapper = styled.section`
-  margin-left: .3rem;
+  margin-left: 0.3rem;
   margin-top: 1rem;
   height: 130px;
   width: 110px;
   // background-color: #58AE3A;
-  background-color: #EB6900;
+  background-color: #eb6900;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 const CardImage = styled.img`
   width: 100%;
   height: 80px;
   border-radius: 10px;
   border: 3px solid #fff;
   object-fit: cover;
-`
+`;
 
 const Type = styled.h2`
   font-size: 1rem;
   color: #f4f4f4;
   margin: 0;
-  margin-top: .5rem;
-`
+  margin-top: 0.5rem;
+`;
 
 const Date = styled.p`
   color: #f4f4f4;
-  font-size: .8rem;
+  font-size: 0.8rem;
   margin: 0;
   padding: 0;
-`
+`;
 
 const LogosWrapper = styled.section`
   display: flex;
@@ -123,24 +123,22 @@ const LogosWrapper = styled.section`
   padding: 1rem;
   width: 100%;
   height: 10vh;
-  
-`
+`;
 
 const Logo = styled.img`
   height: 8vh;
-`
+`;
 
 const Logo2 = styled.img`
   height: 11vh;
-`
+`;
 
 const Home = () => {
-
   const { data: response, isLoading, error } = useGetEventsQuery();
 
   const [events, setEvents] = useState([]);
-  const [page, setPage] = useState(0)
-  const [items, setItems] = useState([])
+  const [page, setPage] = useState(0);
+  const [items, setItems] = useState([]);
 
   // Modal
   const [show, setShow] = useState(false);
@@ -148,183 +146,170 @@ const Home = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [index, setIndex] = useState(0);
-  const [lang, setLang] = useState('')
-
+  const [lang, setLang] = useState('');
 
   useEffect(() => {
-    const lastSelected = JSON.parse(
-      localStorage.getItem("lang") ?? "[]"
-    );
+    const lastSelected = JSON.parse(localStorage.getItem('lang') ?? '[]');
     setLang(lastSelected);
-  }, [])
+  }, []);
 
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
-  };  
-
+  };
 
   function handlePage(index) {
-    setPage(index)
+    setPage(index);
   }
 
   useEffect(() => {
     if (isLoading === false) {
-      setEvents(paginate(response))
+      setEvents(paginate(response));
     }
-  }, [isLoading, response])
+  }, [isLoading, response]);
 
   useEffect(() => {
-    if (events.length) {
-      setItems(events[page])
+    if (!!events.length) {
+      setItems(events[page]);
     }
-  }, [events, page])
+  }, [events, page]);
 
   useEffect(() => {
-    handlePage(0)
-  }, [events.length])
+    handlePage(0);
+  }, [events.length]);
 
   const nextPage = () => {
     setPage((oldPage) => {
-      let nextPage = oldPage + 1
+      let nextPage = oldPage + 1;
       if (nextPage > events.length - 1) {
-        nextPage = 0
+        nextPage = 0;
       }
-      return nextPage
-    })
-  }
+      return nextPage;
+    });
+  };
 
   const prevPage = () => {
     setPage((oldPage) => {
-      let prevPage = oldPage - 1
+      let prevPage = oldPage - 1;
       if (prevPage < 0) {
-        prevPage = events.length - 1
+        prevPage = events.length - 1;
       }
-      return prevPage
-    })
-  }
+      return prevPage;
+    });
+  };
 
+  console.log(events);
 
   return (
-
     <EventsWrapper>
-      {isLoading ? (<Loader />) : error ? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) : (
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
         <>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>
+                {lang === 'Português' ? 'Detalhes' : 'Wahimikë'}
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {!info ? (
+                <Loader />
+              ) : (
+                <>
+                  <Carousel
+                    activeIndex={index}
+                    onSelect={handleSelect}
+                    className='mb-4'
+                  >
+                    <Carousel.Item>
+                      <img
+                        className='d-block w-100'
+                        src={info.image1}
+                        alt='First slide'
+                      />
+                    </Carousel.Item>
 
+                    {!info.image2 ? (
+                      ''
+                    ) : (
+                      <Carousel.Item>
+                        <img
+                          className='d-block w-100'
+                          src={info.image2}
+                          alt='Second slide'
+                        />
+                      </Carousel.Item>
+                    )}
 
-<Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{lang === "Português" ? "Detalhes" : "Wahimikë"}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        { !info ? <Loader /> :  (
+                    {!info.image3 ? (
+                      ''
+                    ) : (
+                      <Carousel.Item>
+                        <img
+                          className='d-block w-100'
+                          src={info.image3}
+                          alt='Third slide'
+                        />
+                      </Carousel.Item>
+                    )}
+                  </Carousel>
 
-
-<>
-<Carousel activeIndex={index} onSelect={handleSelect} className="mb-4">
-
-        
-<Carousel.Item>
-  <img
-    className="d-block w-100"
-    src={info.image1}
-    alt="First slide"
-  />
-</Carousel.Item>
-
-{!info.image2 ? '' : (
-
-  <Carousel.Item>
-  <img
-    className="d-block w-100"
-    src={info.image2}
-    alt="Second slide"
-  />
-  </Carousel.Item>
-
-)}
-
-
-{!info.image3 ? '' : (
-
-<Carousel.Item>
-<img
-  className="d-block w-100"
-  src={info.image3}
-  alt="Third slide"
-/>
-</Carousel.Item>
-
-)}
-
-
-
-
-
-</Carousel>
-
-
-
-
-
-
-        <Table >
-            <tr>
-            <Th>{lang === "Português" ? "Tipo:" : "Kurenaha"}</Th>
-            <Td>{info.complaint}</Td>
-            </tr>
-            <tr>
-            <Th>{lang === "Português" ? "Data:" : "Thë Wakara"}</Th>
-            <Td>{info.date_occurrence.split('-').reverse().join('/')}</Td>
-            </tr>
-            <tr>
-            <Th>Horário:</Th>
-            <Td>{info.time_occurrence}</Td>
-            </tr>
-            <tr>
-            <Th>{lang === "Português" ? "Status:" : "Pei Wãwãhã"}</Th>
-            <Td>{info.status}</Td>
-            </tr>
-            <tr>
-            <Th>{lang === "Português" ? "Descrição:" : "Wahimiai"}</Th>
-            <Td>{info.description}</Td>
-            </tr>
-          </Table>
-
-          </>
-        )}
-
-
+                  <Table>
+                    <tr>
+                      <Th>{lang === 'Português' ? 'Tipo:' : 'Kurenaha'}</Th>
+                      <Td>{info.complaint}</Td>
+                    </tr>
+                    <tr>
+                      <Th>{lang === 'Português' ? 'Data:' : 'Thë Wakara'}</Th>
+                      <Td>
+                        {info.date_occurrence.split('-').reverse().join('/')}
+                      </Td>
+                    </tr>
+                    <tr>
+                      <Th>Horário:</Th>
+                      <Td>{info.time_occurrence}</Td>
+                    </tr>
+                    <tr>
+                      <Th>{lang === 'Português' ? 'Status:' : 'Pei Wãwãhã'}</Th>
+                      <Td>{info.status}</Td>
+                    </tr>
+                    <tr>
+                      <Th>
+                        {lang === 'Português' ? 'Descrição:' : 'Wahimiai'}
+                      </Th>
+                      <Td>{info.description}</Td>
+                    </tr>
+                  </Table>
+                </>
+              )}
             </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Fechar
-          </Button>
-          {/* <Button variant="primary" onClick={handleClose}>
+            <Modal.Footer>
+              <Button variant='secondary' onClick={handleClose}>
+                Fechar
+              </Button>
+              {/* <Button variant="primary" onClick={handleClose}>
             Save Changes
           </Button> */}
-        </Modal.Footer>
-        </Modal>
-
-
-
-
-
+            </Modal.Footer>
+          </Modal>
 
           <Events>
-            {
-              items.map((item, index) => (
-                <Wrapper onClick={() => (handleShow(), setInfo(item))}>
-                <CardImage src={item.image1} alt="Indisponível" />
+            {items.map((item, index) => (
+              <Wrapper onClick={() => (handleShow(), setInfo(item))}>
+                <CardImage src={item.image1} alt='Indisponível' />
                 <Type>{item.complaint}</Type>
-                <Date>{item.date_occurrence.split('-').reverse().join('/')}</Date>
-                </Wrapper>
-              ))
-            }
-
+                <Date>
+                  {item.date_occurrence.split('-').reverse().join('/')}
+                </Date>
+              </Wrapper>
+            ))}
           </Events>
 
           {!!events.length && (
-
             <PageSelector
               pageCount={events}
               items={events[page]}
@@ -334,19 +319,18 @@ const Home = () => {
               pages={events}
               page={page}
             />
-
-            
           )}
           <LogosWrapper>
             <Logo src={conaferLogo} />
             <Logo src={GuardiansLogo} />
             <Logo src={BrigadasLogo} />
             <Logo src={TerraLogo} />
-          </LogosWrapper> 
+          </LogosWrapper>
           {/* <Image /> */}
-        </>)}
+        </>
+      )}
     </EventsWrapper>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
